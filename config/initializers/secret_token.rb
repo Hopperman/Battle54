6408746a03138ff83273745c6c9bbeca72062dd1
Hanno <hanno@platform45.com>
2014-05-08 05:24:08 -0700

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Battle54::Application.config.secret_token = '78ad058cb08522dbe1aa00a3f9ab0396510ff70cd82a08a5aec7dfb88f9ddbc0e73e9bf0b37d09cf44685396980dfa48f97c8b3441a923a0b13bf444aa9ba444'
